739b7f161c8c9d6946ced2c88fcca66f5b66662f
keefertaylor <keefer.t@gmail.com>
2020-08-11 23:26:26 -0700

    wire a linter

--- src/asn1.ts
+++ src/asn1.ts
@@ -1,12 +1,17 @@
-const ASN1 = require('@lapo/asn1js');
+/** Disable some linting rules since the ASN1 library is not written in Typescript. */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const ASN1 = require('@lapo/asn1js')
 
 /** Exports ASN1 with additional functionality */
 
 // Add method to change a DER encoded key to a hex string.
-ASN1.prototype.toHexStringContent = function () {
-    let hex = this.stream.hexDump(this.posContent(), this.posEnd(), true);
-    if (hex.startsWith('00')) hex = hex.slice(2);
-    return hex;
-};
+ASN1.prototype.toHexStringContent = function (): string {
+  const hex = this.stream.hexDump(this.posContent(), this.posEnd(), true)
+  return hex.startsWith('00') ? hex.slice(2) : hex
+}
 
 export default ASN1
--- src/revoke.ts
+++ src/revoke.ts
@@ -1,12 +1,16 @@
 import { LogLevel } from './common'
 import Utils from './utils'
-import { TezosNodeReader, TezosNodeWriter, TezosParameterFormat } from 'conseiljs';
+import {
+  TezosNodeReader,
+  TezosNodeWriter,
+  TezosParameterFormat,
+} from 'conseiljs'
 import Constants from './constants'
-import OperationFeeEstimator from './operation-fee-estimator';
+import OperationFeeEstimator from './operation-fee-estimator'
 
 /**
  * Revoke an Oracle contract.
- * 
+ *
  * @param logLevel The level at which to log output.
  * @param signedRevokeCommand A signature which revokes the Oracle.
  * @param oracleContractAddress The address of the oracle contract.
@@ -14,57 +18,71 @@ import OperationFeeEstimator from './operation-fee-estimator';
  * @param tezosNodeURL A URL of a Tezos node that the operation will be broadcast to.
  */
 export default async function revokeOracle(
-    logLevel: LogLevel,
-    signedRevokeCommand: string,
-    oracleContractAddress: string,
-    revokerPrivateKey: string,
-    tezosNodeURL: string,
-) {
-    try {
-        Utils.print("Revoking oracle contract: " + oracleContractAddress)
+  logLevel: LogLevel,
+  signedRevokeCommand: string,
+  oracleContractAddress: string,
+  revokerPrivateKey: string,
+  tezosNodeURL: string,
+): Promise<void> {
+  try {
+    Utils.print('Revoking oracle contract: ' + oracleContractAddress)
 
-        // Generate a keystore.
-        const keystore = await Utils.keyStoreFromPrivateKey(revokerPrivateKey)
-        const signer = await Utils.signerFromKeyStore(keystore)
-        if (logLevel == LogLevel.Debug) {
-            Utils.print("Revoking from account: " + keystore.publicKeyHash)
-            Utils.print("")
-        }
-
-        // Make the update parameter.
-        const parameter = `"${signedRevokeCommand}"`
-        if (logLevel == LogLevel.Debug) {
-            Utils.print("Made parameter: ")
-            Utils.print(parameter)
-            Utils.print("")
-        }
-
-        const counter = await TezosNodeReader.getCounterForAccount(tezosNodeURL, keystore.publicKeyHash);
-        const entrypoint = 'revoke'
-        const operation = TezosNodeWriter.constructContractInvocationOperation(
-            keystore.publicKeyHash,
-            counter,
-            oracleContractAddress,
-            0,
-            0,
-            Constants.storageLimit,
-            Constants.gasLimit,
-            entrypoint,
-            parameter,
-            TezosParameterFormat.Michelson
-        )
-
-        const operationFeeEstimator = new OperationFeeEstimator(tezosNodeURL)
-        const operationsWithFees = await operationFeeEstimator.estimateAndApplyFees([operation])
-        const nodeResult = await TezosNodeWriter.sendOperation(tezosNodeURL, operationsWithFees, signer)
-
-        Utils.print("Revoked with operation hash: " + nodeResult.operationGroupID.replace(/"/g, ""))
-    } catch (error) {
-        Utils.print("Error occurred while trying to revoke.")
-        if (logLevel == LogLevel.Debug) {
-            Utils.print(error.message)
-        }
-        Utils.print("")
+    // Generate a keystore.
+    const keystore = await Utils.keyStoreFromPrivateKey(revokerPrivateKey)
+    const signer = await Utils.signerFromKeyStore(keystore)
+    if (logLevel == LogLevel.Debug) {
+      Utils.print('Revoking from account: ' + keystore.publicKeyHash)
+      Utils.print('')
     }
 
+    // Make the update parameter.
+    const parameter = `"${signedRevokeCommand}"`
+    if (logLevel == LogLevel.Debug) {
+      Utils.print('Made parameter: ')
+      Utils.print(parameter)
+      Utils.print('')
+    }
+
+    const counter = await TezosNodeReader.getCounterForAccount(
+      tezosNodeURL,
+      keystore.publicKeyHash,
+    )
+    const entrypoint = 'revoke'
+    const operation = TezosNodeWriter.constructContractInvocationOperation(
+      keystore.publicKeyHash,
+      counter,
+      oracleContractAddress,
+      0,
+      0,
+      Constants.storageLimit,
+      Constants.gasLimit,
+      entrypoint,
+      parameter,
+      TezosParameterFormat.Michelson,
+    )
+
+    const operationFeeEstimator = new OperationFeeEstimator(tezosNodeURL)
+    const operationsWithFees = await operationFeeEstimator.estimateAndApplyFees(
+      [operation],
+    )
+    const nodeResult = await TezosNodeWriter.sendOperation(
+      tezosNodeURL,
+      operationsWithFees,
+      signer,
+    )
+
+    Utils.print(
+      `Revoked with operation hash: ${nodeResult.operationGroupID.replace(
+        /"/g,
+        '',
+      )}`,
+    )
+  } catch (error) {
+    Utils.print('Error occurred while trying to revoke.')
+    if (logLevel == LogLevel.Debug) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      Utils.print(error.message)
+    }
+    Utils.print('')
+  }
 }
